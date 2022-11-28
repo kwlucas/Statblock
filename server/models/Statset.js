5941234c1989f21d3195bc293943f7734bef8f24
kwlucas <lucaswkyle@gmail.com>
2022-11-28 09:34:43 -0700

    Added abilities and skills

--- server/models/Statset.js
+++ server/models/Statset.js
@@ -32,4 +32,86 @@ const statsetSchema = new Schema({
             default: 'walking',
         }
     },
+    abilities: {
+        strength: Number,
+        dexterity: Number,
+        constitution: Number,
+        intelligence: Number,
+        wisdom: Number,
+        charisma: Number,
+    },
+    skills: {
+        acrobatics: {
+            type: String,
+            default: 'none'//Either 'none', 'proficient', or 'expert' 
+        },
+        animalHandling: {
+            type: String,
+            default: 'none'
+        },
+        arcana: {
+            type: String,
+            default: 'none'
+        },
+        athletics: {
+            type: String,
+            default: 'none'
+        },
+        deception: {
+            type: String,
+            default: 'none'
+        },
+        history: {
+            type: String,
+            default: 'none'
+        },
+        insight: {
+            type: String,
+            default: 'none'
+        },
+        intimidation: {
+            type: String,
+            default: 'none'
+        },
+        investigation: {
+            type: String,
+            default: 'none'
+        },
+        medicine: {
+            type: String,
+            default: 'none'
+        },
+        nature: {
+            type: String,
+            default: 'none'
+        },
+        perception: {
+            type: String,
+            default: 'none'
+        },
+        performance: {
+            type: String,
+            default: 'none'
+        },
+        persuasion: {
+            type: String,
+            default: 'none'
+        },
+        religion: {
+            type: String,
+            default: 'none'
+        },
+        sleightOfHand: {
+            type: String,
+            default: 'none'
+        },
+        stealth: {
+            type: String,
+            default: 'none'
+        },
+        survival: {
+            type: String,
+            default: 'none'
+        },
+    },
 });
