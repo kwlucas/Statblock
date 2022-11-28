1a000bba41572579ac4e5b63a808cd6be962a666
kwlucas <lucaswkyle@gmail.com>
2022-11-28 09:21:53 -0700

    Formatting

--- server/models/User.js
+++ server/models/User.js
@@ -3,41 +3,41 @@ const bcrypt = require('bcrypt');
 
 const userSchema = new Schema({
     username: {
-      type: String,
-      required: true,
-      unique: true,
-      trim: true,
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
     },
     email: {
-      type: String,
-      required: true,
-      unique: true,
+        type: String,
+        required: true,
+        unique: true,
     },
     password: {
-      type: String,
-      required: true,
+        type: String,
+        required: true,
     },
     characters: [
         {
-          type: Schema.Types.ObjectId,
-          ref: 'Character',
+            type: Schema.Types.ObjectId,
+            ref: 'Character',
         },
     ],
 });
 
 userSchema.pre('save', async function (next) {
     if (this.isNew || this.isModified('password')) {
-      const salt = 10;
-      this.password = await bcrypt.hash(this.password, salt);
+        const salt = 10;
+        this.password = await bcrypt.hash(this.password, salt);
     }
-  
+
     next();
 });
-  
-  userSchema.methods.checkPassword = async function (password) {
-    return bcrypt.compare(password, this.password);
-  };
-  
-  const User = model('user', userSchema);
 
-  module.exports = User;
+userSchema.methods.checkPassword = async function (password) {
+    return bcrypt.compare(password, this.password);
+};
+
+const User = model('user', userSchema);
+
+module.exports = User;
