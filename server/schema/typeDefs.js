65d5c1460a62d42915ca4868817e2f0972431779
kwlucas <lucaswkyle@gmail.com>
2022-11-29 19:18:46 -0700

    Statset typedefs created

--- server/schema/typeDefs.js
+++ server/schema/typeDefs.js
@@ -8,9 +8,68 @@ const typeDefs = gql`
     description: String
     statset: [Statset]
     }
+
     type Statset {
     _id: ID
-    
+    type: String
+    class: String
+    background: String
+    level: Int
+    status: String
+    health: {
+      hp: Int
+      mhp: Int
+    }
+    ac: Int
+    movement: {
+      walking: Int
+      flying: Int
+      climbing: Int
+      swimming: Int
+      primary: String
+    }
+    abilities: {
+      strength: Int
+      dexterity: Int
+      constitution: Int
+      intelligence: Int
+      wisdom: Int
+      charisma: Int
+    }
+    skills: {
+      acrobatics: String
+      animalHandling: String
+      arcana: String
+      athletics: String
+      deception: String
+      history: String
+      insight: String
+      intimidation: String
+      investigation: String
+      medicine: String
+      nature: String
+      perception: String
+      performance: String
+      persuasion: String
+      religion: String
+      sleightOfHand: String
+      stealth: String
+      survival: String
+    }
+    spellSlots: [Int]
+    traits: [
+      {
+        feature: String
+        details: String
+        priority: Int
+      }
+    ]
+    inventory: [{
+      item: String
+      quantity: Int
+      details: String
+      priority: Int
+    }]
   }
 
   type User {
@@ -29,7 +88,7 @@ const typeDefs = gql`
     name: String
     race: String
     description: String
-    statset: [Statset]
+    statset: Statset
   }
   input Statset {
       _id: ID
@@ -38,11 +97,9 @@ const typeDefs = gql`
 
 
   type Query {
-    
     users: [Users]
     user(id: ID!): User
     statset(id: ID!): Statset
-
   }
 
   type Mutation {
