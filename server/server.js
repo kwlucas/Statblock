f9dca4ac865de04ab5fef346b4472024b170ae19
Brendon Allen Curry-Hobbs <46946074+brendonc-h@users.noreply.github.com>
2022-11-29 20:43:16 -0700

    server error fix

--- server/server.js
+++ server/server.js
@@ -18,5 +18,5 @@ if (process.env.NODE_ENV === 'production') {
 app.use(routes);
 
 db.once('open', () => {
-    app.listen(PORT, () => console.log(`Now listening on localhost:${PORT}`);)
+    app.listen(PORT, () => console.log(`Now listening on localhost:${PORT}`));
 })
