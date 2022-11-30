1cdd9e7ed3ebbce7191c067362826d9390ca70b8
Drew Lovato <gaethlovato@gmail.com>
2022-11-29 20:33:23 -0700

    updating client side

---
+++ client/src/components/FooterNavBar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function FooterNavBar() {
+  return (
+    <div className="footer-section">
+      <ul className="footer-ul">
+        <a href="/">DnD</a>
+        <a href="/">GitHub</a>
+        <a href="/">Readit</a>
+      </ul>
+    </div>
+  );
+}
+
+export default FooterNavBar;
