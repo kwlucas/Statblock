245226f9bcb0c75b9c80e8977f8140ebdb7df864
kwlucas <lucaswkyle@gmail.com>
2022-12-04 17:19:58 -0700

    Debugging

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -4,6 +4,9 @@ export const CREATE_USER = gql`
 mutation createUser($username: String!, $email: String!, $password: String!) {
   createUser(username: $username, email: $email, password: $password) {
     token
+    user {
+      _id
+    }
   }
 }
 `;
@@ -12,6 +15,9 @@ export const LOGIN_USER = gql`
 mutation login($email: String!, $password: String!) {
   login(email: $email, password: $password) {
     token
+    user {
+      _id
+    }
   }
 }
 `;
